33afe304a786e06a1b2efa280555628524f8da3f
Yina Zhu <yinagit@gmail.com>
2018-06-03 00:08:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,24 +2,35 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-    get '/' do
-      erb :root
+
+    get '/pirates' do
+      @pirates = Pirate.all
+
+      erb :'pirates/index'
     end
 
-    get '/new' do
+    get '/pirates/new' do #don't do just /new, be specific because in this app there's pirate and ships
       erb :'pirates/new'
     end
 
-    post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-      @ships = Ship.all
+    get '/pirates/:id' do #'/:id' is known as the dynamic segment
+      @pirate = Pirate.find(params[:id])
 
       erb :'pirates/show'
     end
+
+    post '/pirates' do
+      # binding.pry
+      pirate = Pirate.new(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:weight])
+      params[:pirate][:ships].each do |ship_data|
+        ship = Ship.new(ship_data)
+        ship.pirate = pirate
+        ship.save
+      end
+      # binding.pry
+      redirect to "pirates/#{pirate.id}"
+    end
+
 
   end
 end
